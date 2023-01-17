e044bf6cd8c76405207c0bad9f7a77eb6b64b0b0
Tomás Mota <tmm@danskecommodities.com>
2023-01-17 08:19:54 +0100

    zsh
    Signed-off-by: Tomás Mota <tmm@danskecommodities.com>

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,19 @@
+# Quick file edit
+alias editrc="chezmoi edit $XDG_CONFIG_HOME/zsh/.zshrc"
+alias loadrc="source ~/.zshrc"
+
+# Git
+alias gc='git commit --signoff -m'
+alias gpt='git push --tags'
+
+# Terraform
+alias tfi='terraform init'
+alias tfp='terraform plan'
+alias tfa='terraform apply'
+alias tfd='terraform destroy'
+alias tfs='terraform show'
+
+# Generic
+alias nv='nvim'
+alias k='kubectl'
+alias cm='chezmoi'
