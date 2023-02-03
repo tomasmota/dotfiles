6a23fbc23b3b6ddd4ebba0f5187f7a11aec189c1
Tomás Mota <tmm@danskecommodities.com>
2023-02-03 07:54:52 +0100

    auto

--- dot_config/zsh/functions.zsh
+++ dot_config/zsh/functions.zsh
@@ -3,3 +3,10 @@ function delete_tag(){
     git tag -d $1
     git push --delete origin $1
 }
+
+# push dotfiles
+function cmpush(){
+    chezmoi git add .
+    chezmoi git -- commit -m "auto"
+    chezmoi git push
+}
